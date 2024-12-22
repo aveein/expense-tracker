cdb63c0a91d17bbb87528783d7c0865960f25645
Avin Amatya <avin@Avins-Mac-mini.local>
2024-12-22 12:49:18 +0545

    added auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect('login');
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('categories',CategoryController::class);
